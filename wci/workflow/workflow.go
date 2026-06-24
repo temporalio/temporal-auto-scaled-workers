fd496dbfb749a2980e7fc99248b4893c9622bd97
Stefan Richter <stefan.richter@temporal.io>
2026-06-24 12:23:53 -0700

    Add deployment name and build it to WF logs
    The WF logs are missing the deployment name and build ID making it hard to debug things.

--- wci/workflow/workflow.go
+++ wci/workflow/workflow.go
@@ -84,7 +84,7 @@ func Workflow(ctx workflow.Context, unsafeWorkflowVersionGetter func() WorkerCon
 		WorkerControllerInstanceWorkflowArgs: args,
 		workflowVersion:                      getWorkflowVersion(ctx, unsafeWorkflowVersionGetter),
 		a:                                    activities,
-		logger:                               sdklog.With(workflow.GetLogger(ctx), "wf-namespace", args.NamespaceName),
+		logger:                               sdklog.With(workflow.GetLogger(ctx), "wf-namespace", args.NamespaceName, "wf-deployment-name", args.DeploymentName, "wf-build-id", args.BuildId),
 		metrics: workflow.GetMetricsHandler(ctx).WithTags(map[string]string{
 			wcimetrics.NamespaceTag:               args.NamespaceName,
 			wcimetrics.WorkerDeploymentNameTag:    args.DeploymentName,
