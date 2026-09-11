7ffde755ccc88b8425fc07c27f84d5d24a5fc2b0
Tyler Cunnion <tyler.cunnion@temporal.io>
2026-09-11 09:17:59 -0700

    Bump default WCI version to 3

--- wci/client/config.go
+++ wci/client/config.go
@@ -32,7 +32,7 @@ var (
 	)
 	WorkerControllerInstanceWorkflowVersion = dynamicconfig.NewNamespaceIntSetting(
 		"workercontroller.instanceWorkflowVersion",
-		2,
+		3,
 		`WorkerControllerInstanceWorkflowVersion controls what version of the logic should the manager workflows use.`,
 	)
 	WorkerControllerMinSignalIntervalNoSyncMatchMilliseconds = dynamicconfig.NewNamespaceIntSetting(
